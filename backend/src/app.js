cda2b81f1d07ece83b6ea6e6a2e9262e4e3cfa1f
HiagoDz7 <levexhiago@gmail.com>
2026-08-14 13:40:02 -0300

    Primeiros Arquivos

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import Produtos from "./routes/productRoutes.js"
+
+const app = express();
+app.use(express.json());
+app.use("/produtos", productRoutes);
+app.get( '/funcional', (req, res) =>{
+    res.send('funcionando')
+})
+
+
+export default app;
